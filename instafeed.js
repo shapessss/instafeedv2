4055cebc94b334295abec06eb9f6e406970ea689
Jordan Delgado <jordelquino@gmail.com>
2022-06-13 16:04:27 -0500

    Added new function and await states

--- instafeed.js
+++ instafeed.js
@@ -1,35 +1,44 @@
 const accessToken = "IGQVJXOTlPZAF9LZAmd1RzFPT1ZA6alQyYXA0LXkwMkxrbEg0OHFTWExiSVVGemQ3d05XeFBKUDREZADlTLUdacllxRV83X2xnWjNhcHVLU3JEM1V4LTk5T0VYeUhHN29MVUJWdUliN0tMTUthMzVXNWNicAZDZD"
 const fields = "id,media_type,media_url,timestamp,permalink"
-const apiUrl = `https://graph.instagram.com/me/media?fields=${fields}=&access_token=${accessToken}`
 const section = document.querySelector("section")
+
+const refreshAccessToken = async () => {
+    const superhiApi = `https://api.superhi.com/api/test/token/instagram?access_token=${accessToken}`
+    const response = await fetch(superhiApi)
+    const {access_Token} = await response.json()
+    return accessToken
+}
 
 const fetchPosts = async () => {
     try {
+    const newToken = await refreshAccessToken()
+    const apiUrl = `https://graph.instagram.com/me/media?fields=${fields}=&access_token=${newToken}`
     const response = await fetch(apiUrl)
     const {data} = await response.json()
 
+    section.innerHTML = ""
     data.forEach(post => {
         let a = document.createElement("a")
         a.href = post.permalink
         a.target = "_blank"
         a.rel = "noreferrer noopener"
 
+        let container
         if (post.media_type === "VIDEO") {
-            let video = document.createElement("video")
-            video.src = post.media_url
-            video.preload = true
-            video.autoplay = true
-            video.muted = true
-            video.loop = true
-            a.appendChild(video)
+            container = document.createElement("video")
+            container.preload = true
+            container.autoplay = true
+            container.muted = true
+            container.loop = true
         } else {
-            let img = document.createElement("img")
-            img.src = post.media_url
-            a.appendChild(img)
+            container = document.createElement("img")
         }
+        container.src = post.media_url
+        a.appendChild(container)
         section.appendChild(a)
     })
     } catch (error) {
+        section.innerHTML = "Cannot fetch posts"
         console.error(error)
     }
 }
